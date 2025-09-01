15d49a433ce1a0f5540f90adb030e7ee82e75f23
Dinesh Budhathoki <physmarika@gmail.com>
2025-09-02 01:13:05 +0800

    Vite config is fixed

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "./", // Important for S3 static hosting
+  base: "/", // Important for S3 static hosting
 });
